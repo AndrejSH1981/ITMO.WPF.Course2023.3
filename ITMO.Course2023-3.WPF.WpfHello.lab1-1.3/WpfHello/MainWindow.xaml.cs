d40b6706a3ad0ea7aa906bdb2441297d74794e9e
AndrejSH1981 <133521547+AndrejSH1981@users.noreply.github.com>
2023-06-03 23:52:51 +0300

    Add files via upload

---
+++ ITMO.Course2023-3.WPF.WpfHello.lab1-1.3/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object username1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button1.IsEnabled = false;
+            Button2.IsEnabled = false;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter("D:\\username1.txt");
+                sw.WriteLine(myTextBox1.Text);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+            Button2.IsEnabled = true;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("D:\\username1.txt");
+                myLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Button1.IsEnabled = true;
+        }
+
+    }
+    }
+
