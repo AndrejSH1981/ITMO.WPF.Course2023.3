8fc01c49ffaefccc1a4136e4a5a60dbc17d10d0c
AndrejSH1981 <133521547+AndrejSH1981@users.noreply.github.com>
2023-06-06 20:51:37 +0300

    Add files via upload

---
+++ ITMO.WpfCS.Cours2023-3.lab9.DataBindingDemo/ITMO.WpfCS.Cours2023-3.lab9.DataBindingDemo/BoolToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace ITMO.WpfCS.Cours2023_3.lab9.DataBindingDemo
+{
+    [ValueConversion(typeof(bool), typeof(Brush))]
+    public class BoolToBrushConverter : IValueConverter
+    //class BoolToBrushConverter
+    //{
+    //}
+
+
+    {
+        public object Convert(object value,Type targetType,object parameter,CultureInfo culture)
+        {
+            Brush b = null;
+            if (value != null &&value.GetType() == typeof(bool))
+            {
+                b = (bool)value ? Brushes.Green : Brushes.Red;
+            }
+            return b;
+        }
+        public object ConvertBack(object value,Type targetType,object parameter,CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+}
---
+++ ITMO.WpfCS.Cours2023-3.lab9.DataBindingDemo/ITMO.WpfCS.Cours2023-3.lab9.DataBindingDemo/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ITMO.WpfCS.Cours2023_3.lab9.DataBindingDemo
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+
+    public class StudentList : ObservableCollection<Student>
+    {
+    }
+
+}
+
+
